5730498edca2e82d999589f8007328ec8bcef5e6
Francisco Dutra <113071795+frandutra@users.noreply.github.com>
2025-02-24 20:58:20 -0300

    Reapply "bbd.service deberian funcionar"
    This reverts commit 54e20e12a7721603276db94a48ebe1a62a4e50aa.

--- dacs2023-fe/src/app/core/services/bdd.service.ts
+++ dacs2023-fe/src/app/core/services/bdd.service.ts
@@ -1,8 +1,9 @@
-// bdd.service.ts
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { BddResponse } from '../models/bdd.model';  // Asegúrate de importar el modelo correctamente
+import { AuthService } from './auth.service';
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +11,20 @@ import { BddResponse } from '../models/bdd.model';  // Asegúrate de importar el
 export class BddService {
 
   private apiUrl = 'http://localhost:9001/bff/backend/customer';  // Reemplaza con tu endpoint
+  private token: string | null = null;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private authService: AuthService) {
+    this.authService.getToken().subscribe(token => {
+      this.token = token;
+    });
+  }
+
 
   getBddData(): Observable<BddResponse> {
-    return this.http.get<BddResponse>(this.apiUrl);
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${this.token}`
+    });
+
+    return this.http.get<BddResponse>(this.apiUrl, { headers });
   }
 }
