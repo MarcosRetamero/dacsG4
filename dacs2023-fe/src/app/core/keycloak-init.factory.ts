6bf8daf9783c2c8ebc15e7ba768eac4208233a2b
Marcos Retamero <marcosretamero2@gmail.com>
2025-03-28 22:15:17 -0300

    Update keycloak-init.factory.ts

--- dacs2023-fe/src/app/core/keycloak-init.factory.ts
+++ dacs2023-fe/src/app/core/keycloak-init.factory.ts
@@ -6,7 +6,7 @@ export function initializeKeycloak(keycloak: KeycloakService): () => Promise<boo
       .init({
         config: {
           url: 'http://localhost:8080',
-          realm: 'dacsgrupo4',
+          realm: 'master',
           clientId: 'dacs2023-fe',
         },
         initOptions: {
